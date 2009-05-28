3f5e794b4b232b9d0853516e9871c314d7d5985d
viirya <viirya@3f650d92-3fc9-11de-8624-41c2277f587d>
2009-05-28 05:39:06 +0000

    Now ZhuYin IME takes its database file from assets/ and copies to system database path, so no need to manually push database for users.

--- ZhuYinIME/src/tw/cheyingwu/zhuyin/ZhuYinDictionaryProvider.java
+++ ZhuYinIME/src/tw/cheyingwu/zhuyin/ZhuYinDictionaryProvider.java
@@ -1,16 +1,24 @@
 package tw.cheyingwu.zhuyin;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 
+import android.app.AlertDialog;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.util.Log;
+import android.widget.Toast;
 
 public class ZhuYinDictionaryProvider extends ContentProvider {
 	private static final String TAG = "ZhuYinIME";
@@ -27,9 +35,14 @@ public class ZhuYinDictionaryProvider extends ContentProvider {
 	 * This class helps open, create, and upgrade the database file.
 	 */
 	private static class DatabaseHelper extends SQLiteOpenHelper {
+		
+		private static String DB_PATH = "/data/data/tw.cheyingwu.zhuyin/databases/";
+		private final Context myContext;
+
 
 		DatabaseHelper(Context context) {
 			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+			this.myContext = context;
 		}
 
 		@Override
@@ -52,6 +65,92 @@ public class ZhuYinDictionaryProvider extends ContentProvider {
 			db.execSQL("DROP TABLE IF EXISTS notes");
 			onCreate(db);
 		}
+		
+		/**
+	     * Creates a empty database on the system and rewrites it with your own database.
+	     * */
+	    public void createDataBase() throws IOException{
+	 
+	    	boolean dbExist = checkDataBase();
+	 
+	    	if (dbExist) {
+	    		//do nothing - database already exist
+	    	} else {
+	 
+	    		//By calling this method and empty database will be created into the default system path
+	            //of your application so we are gonna be able to overwrite that database with our database.	    		
+	        	this.getReadableDatabase();
+	 
+	        	try {
+	    			copyDataBase();
+	
+	    		} catch (IOException e) {
+	 
+	        		throw new Error("Error copying database");
+	 
+	        	}
+	    	}
+	 
+	    }
+
+		
+		/**
+	     * Check if the database already exist to avoid re-copying the file each time you open the application.
+	     * @return true if it exists, false if it doesn't
+	     */
+	    private boolean checkDataBase(){
+	 
+	    	SQLiteDatabase checkDB = null;
+	 
+	    	try {
+	    		String myPath = DB_PATH + DATABASE_NAME;
+	    		checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+	 
+	    	} catch (SQLiteException e){	 
+	    		Log.i(TAG, "database does't exist yet");
+	    	}
+	 
+	    	if (checkDB != null) {	 
+	    		checkDB.close();	 
+	    	}
+	 
+	    	return checkDB != null ? true : false;
+	    }
+	    
+	    /**
+	     * Copies your database from your local assets-folder to the just created empty database in the
+	     * system folder, from where it can be accessed and handled.
+	     * This is done by transfering bytestream.
+	     * */
+	    private void copyDataBase() throws IOException {
+	    	
+	    	Log.i(TAG, "start to copy database");
+	 
+	    	//Open your local db as the input stream
+	    	InputStream myInput = myContext.getAssets().open(DATABASE_NAME);
+	 
+	    	// Path to the just created empty db
+	    	String outFileName = DB_PATH + DATABASE_NAME;
+	 
+	    	//Open the empty db as the output stream
+	    	OutputStream myOutput = new FileOutputStream(outFileName);
+	 
+	    	//transfer bytes from the inputfile to the outputfile
+	    	byte[] buffer = new byte[1024];
+	    	int length;
+	    	while ((length = myInput.read(buffer))>0){
+	    		myOutput.write(buffer, 0, length);
+	    	}
+	    	
+	    	Log.i(TAG, "copy database done");
+	 
+	    	//Close the streams
+	    	myOutput.flush();
+	    	myOutput.close();
+	    	myInput.close();
+	 
+	    }
+	
 	}
 
 	private DatabaseHelper mOpenHelper;
@@ -59,6 +158,14 @@ public class ZhuYinDictionaryProvider extends ContentProvider {
 	public ZhuYinDictionaryProvider(Context ctx) {
 		this.context = ctx;
 		mOpenHelper = new DatabaseHelper(this.context);
+							
+		try {			
+			mOpenHelper.createDataBase();
+		} catch (IOException ioe) { 
+			//throw new Error("Unable to create database");
+			Toast.makeText(ctx, R.string.db_create_error, Toast.LENGTH_SHORT).show();			
+		}
+
 	}
 
 	@Override
@@ -82,7 +189,9 @@ public class ZhuYinDictionaryProvider extends ContentProvider {
 	@Override
 	public boolean onCreate() {
 		Log.i(TAG, "CREATE TABLE");
+		
 		mOpenHelper = new DatabaseHelper(getContext());
+		
 		return true;
 	}
 
